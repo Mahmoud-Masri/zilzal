eb5e7e8a80db5f47c6c7c8c63c952f7e8d3a30e1
GT Acc <87899780+GT-ACC@users.noreply.github.com>
2023-02-09 16:24:23 +0300

    handle errors 😓

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getCollection } from "./mongo";
+
 export const api = Router();
 
 api.post("/request-help", async (req, res) => {
@@ -18,9 +19,9 @@ api.post("/provide-help", async (req, res) => {
     try {
         const collection = await getCollection("providers");
         const data = req.body;
-        console.log("data", data)
+        console.log("data", data);
         await collection.insertOne(data);
-        console.log("data", data)
+        console.log("data", data);
         res.send({ ok: true });
     } catch (e) {
         console.log(e);
@@ -29,21 +30,35 @@ api.post("/provide-help", async (req, res) => {
 });
 
 api.get("/requests", async (req, res) => {
-    const collection = await getCollection("requests");
-    const requests = await collection.find().toArray();
-    res.send(requests);
+    try {
+        const collection = await getCollection("requests");
+        const requests = await collection.find().toArray();
+        res.send(requests);
+    } catch (e) {
+        console.log(e);
+        res.send({ ok: false });
+    }
 });
 
 api.get("/providers", async (req, res) => {
-    const collection = await getCollection("providers");
-    const providers = await collection.find().toArray();
-    res.send(providers);
+    try {
+        const collection = await getCollection("providers");
+        const providers = await collection.find().toArray();
+        res.send(providers);
+    } catch (e) {
+        console.log(e);
+        res.send({ ok: false });
+    }
 });
 
 api.get("/requests/:requestId", async (req, res) => {
-
-    const collection = await getCollection("requests");
-    await collection.updateOne({_id: req.params.requestId}, req.body);
-    const request = await collection.findOne({_id: req.params.requestId})
-    res.send(request);
+    try {
+        const collection = await getCollection("requests");
+        await collection.updateOne({ _id: req.params.requestId }, req.body);
+        const request = await collection.findOne({ _id: req.params.requestId });
+        res.send(request);
+    } catch (e) {
+        console.log(e);
+        res.send({ ok: false });
+    }
 });
